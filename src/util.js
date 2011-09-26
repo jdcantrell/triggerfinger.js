00ad7e924ce31bad3bd6e15cd529d15407cd839c
JD Cantrell <jdcantrell@gmail.com>
2011-09-26 14:58:50 -0700

    Add an array remove function for removing items in the array without leaving undefined holes

--- src/util.js
+++ src/util.js
@@ -5,3 +5,10 @@ var logError = function (msg) {
   console.warn('error trace');
   console.trace();
 }
+
+// Array Remove - By John Resig (MIT Licensed)
+var removeItem = function(array, from, to) {
+  var rest = array.slice((to || from) + 1 || array.length);
+  array.length = from < 0 ? array.length + from : from;
+  return array.push.apply(array, rest);
+};
