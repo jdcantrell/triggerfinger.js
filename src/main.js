53397440678cded1077c937c0e976c193f318b40
JD Cantrell <jdcantrell@gmail.com>
2011-09-26 22:43:33 -0700

    Clean up main.js a little, and target a specific div instead of html for initial bind testing

--- src/main.js
+++ src/main.js
@@ -2,33 +2,30 @@
  Assumes jQuery is included already. Will override various event binding/triggering routines.
 */
 
-var bindCallback = function(ctx, target, args, result) {
-      console.log(ctx);
-      BindTable.add({
-        eventName: args[1],
-        selector: 'test',
-        fn: 'test',
-        listenMethod: 'test'
-      });
-    },
-    interception = new Interception({ preInterception: bindCallback });
 
-jQuery.fn.bind = interception.intercept(jQuery.fn.bind);
+var bindInterceptor = new Interception({
+  postInterception: function (ctx, target, args, result) {
+    var data = ctx.data();
+    console.log(data);
+    console.log(ctx.data('events'), args);
+    BindTable.add({
+      eventName: args[0],
+      selector: 'test',
+      fn: args[1],
+      listenMethod: 'bind'
+    });
+  }
+});
+
+//replace jquery functions with our intercepts, preserving the originals
+var bindFn = jQuery.fn.bind;
+jQuery.fn.extend({bindOriginal: jQuery.fn.bind, bind: function () {}});
+jQuery.fn.extend({bind: bindInterceptor.intercept(bindFn)});
 
 var test = {};
 
 $(document).ready(function () {
-  $('html').bind('bsevent', function () {
+  $('#tester').bind('testEvent', function () {
     console.log('this is the callback');
   });
-  
-  $('html').one('onebsevent', function () {
-    console.log('this is the callback');
-  });
-  
-  $('html').live('liveevent', function () {
-    console.log('live event');
-  });
-  
-  $('html').delegate('click', 'div', function () {});
 });
