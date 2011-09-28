4fe02485163e43e9e3f084dfd01201f342099ca7
JD Cantrell <jdcantrell@gmail.com>
2011-09-27 21:31:43 -0700

    Start collecting the bind report data by selector and event

--- src/report.js
+++ src/report.js
@@ -1,8 +1,21 @@
 function bindReport() {
   console.group("Captured binds");
+  var selectors = {};
   BindTable.each(function (idx, record) {
     console.log( record.selector, record.eventName,record.guid, record.fn,record.listenMethod);
+
+    if (typeof selectors[record.selector] === "undefined")
+    {
+      selectors[record.selector] = {};
+    }
+    if (typeof selectors[record.selector][record.eventName] === "undefined")
+    {
+      selectors[record.selector][record.eventName] = [];
+    }
+    
+    selectors[record.selector][record.eventName].push(record);
+
   });
   console.groupEnd("Captured binds");
-
+  console.log(selectors); 
 }
