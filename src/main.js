30dc0f6db1fbe95e7351f8139d96b340d247d940
JD Cantrell <jdcantrell@gmail.com>
2011-09-27 21:29:48 -0700

    Remove debug consoles, also remove the events data check in bind post intercept

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@
 
 var bindInterceptor = new Interception({
   postInterception: function (ctx, targetFn, args, result) {
-    var data = ctx.data();
+    fn = args[args.length - 1];
     BindTable.add({
       eventName: args[0],
       selector: ctx.selector,
-      fn: args[1],
-      guid: args[1].guid,
+      fn: fn,
+      guid: fn.guid,
       listenMethod: 'bind'
     });
   }
@@ -19,18 +19,21 @@ var triggerInterceptor = new Interception({
   postInterception: function (ctx, targetFn, args, result) {
     var data = ctx.data();
     var eventHandlers = ctx.data('events');
-    var i = eventHandlers[args[0]].length;
-
-    while (i--)
+    if (typeof eventHandlers !== "undefined" && typeof eventHandlers[args[0]] !== 'undefined')
     {
-      TriggerTable.add({
-        eventName: args[0],
-        selector: ctx.selector,
-        target: ctx[0],
-        fn: eventHandlers[args[0]][i].handler,
-        guid: eventHandlers[args[0]][i].guid,
-        count: 1
-      });
+      var i = eventHandlers[args[0]].length;
+
+      while (i--)
+      {
+        TriggerTable.add({
+          eventName: args[0],
+          selector: ctx.selector,
+          target: ctx[0],
+          fn: eventHandlers[args[0]][i].handler,
+          guid: eventHandlers[args[0]][i].guid,
+          count: 1
+        });
+      }
     }
   }
 });
@@ -41,21 +44,22 @@ jQuery.fn.extend({bindOriginal: jQuery.fn.bind, bind: function () {}});
 jQuery.fn.extend({bind: bindInterceptor.intercept(bindFn)});
 
 var triggerFn = jQuery.fn.trigger;
-jQuery.fn.extend({triggerOriginal: jQuery.fn.trigger, trigger: function () {}});
-jQuery.fn.extend({trigger: triggerInterceptor.intercept(triggerFn)});
+//jQuery.fn.extend({triggerOriginal: jQuery.fn.trigger, trigger: function () {}});
+//jQuery.fn.extend({trigger: triggerInterceptor.intercept(triggerFn)});
 
 //setup our event data tables
 var BindTable = new DataTable(['eventName', 'selector', 'fn', 'guid', 'listenMethod']);
 BindTable.createIndex(['eventName', 'guid']);
+
 var TriggerTable = new DataTable(['eventName', 'target', 'fn', 'guid', 'count']);
 TriggerTable.createIndex(['eventName', 'guid']);
 
 //table debugging messages
 $(BindTable).bind('add', function (event, record) {
-  console.log('Bind logged:', record.eventName, record.guid, record.selector, record.listenMethod, record);
+  //console.log('Bind logged:', record.eventName, record.guid, record.selector, record.listenMethod, record);
 });
 
 $(TriggerTable).bind('add', function (event, record) {
-  console.log('Trigger logged:', record.eventName, record.guid, record.listenMethod, record);
+  //console.log('Trigger logged:', record.eventName, record.guid, record.listenMethod, record);
 });
 
