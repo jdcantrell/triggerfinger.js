8375f7de86f06f04a6c6388f38741a3fb1c1ad30
JD Cantrell <jdcantrell@gmail.com>
2011-09-27 20:16:11 -0700

    Add build script for combining and minifying, relies on closure compiler

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cat src/util.js src/interception.js src/data.js src/main.js src/report.js > triggerFinger.js
+
+java -jar ../closure-compiler/compiler.jar --js src/util.js --js src/interception.js --js src/data.js --js src/main.js --js src/report.js --js_output_file triggerFinger-min.js
