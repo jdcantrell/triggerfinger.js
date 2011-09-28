62c96b1c94c37f353ddb7190f0f4a919b875a83e
JD Cantrell <jdcantrell@gmail.com>
2011-09-27 17:05:37 -0700

    Add a reporting spec for testing overall functionality

--- tests/spec/ReportSpec.js
+++ tests/spec/ReportSpec.js
@@ -5,11 +5,20 @@ describe("Reporting", function() {
 	  test.bind('blah', function () {
 	    expect(true).toEqual(true);
 	  });
+    function testFn() {
+	    expect(false).toEqual(false);
+    }
+	  test.bind('blah', testFn);
 	  
+	  test.trigger('blah');
 	  test.trigger('blah');
 	});
 	
 	it("records when bind is used", function () {
-	  expect(BindTable.count()).toEqual(1);
+	  expect(BindTable.count()).toEqual(2);
+	});
+
+	it("records when trigger is used", function () {
+	  expect(TriggerTable.count()).toEqual(4);
 	});
 });
