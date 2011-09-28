ffae67c0470d34f1a63a25b571d6af0c4a4620c9
JD Cantrell <jdcantrell@gmail.com>
2011-09-27 17:04:49 -0700

    Update DataTables to support indexing and multi-key get

--- src/data.js
+++ src/data.js
@@ -1,19 +1,44 @@
 var DataTable = function (fields) {
   var table = [];
+  var indexes = [];
+  var indexTables = {};
 
   return {
     add: function (record) {
       var i = fields.length;
       while (i--)
       {
-        if (typeof record[fields[i]] == 'undefined' || record[fields[i]] === null)
+        if (typeof record[fields[i]] == 'undefined' && record[fields[i]] === null)
         {
           logError('Incomplete record given to DataTable', record, 'missing', fields[i]);
           return 0;
         }
       }
 
+      //add to complete table
       table.push(record);
+
+      //add to index
+      indexTable = indexTables;
+      for (var i = 0; i < indexes.length; i += 1)
+      {
+        for (var j = 0; j < indexes[i].length; j += 1)
+        {
+          if (typeof indexTable[indexes[i][j]] === "undefined")
+          {
+            if (j < indexes[i].length - 1)
+            {
+              indexTable[indexes[i][j]] = {}
+            }
+            else 
+            {
+              indexTable[indexes[i][j]] = []
+            }
+          }
+          indexTable = indexTable[indexes[i][j]];
+        }
+        indexTable.push(record);
+      }
 
       $(this).triggerHandler('add', record);
 
@@ -39,12 +64,43 @@ var DataTable = function (fields) {
 
       return affected;
     }, 
-    get: function (key, value) {
+    get: function (keyVal) {
+      var fields = [];
+      for (i in keyVal) {
+        fields.push(i);
+      }
+
+      //see if there is an index record
+      var indexTable = indexTables;
+      for(i = 0; i < fields.length; i += 1)
+      {
+        if (typeof indexTable[fields[i]] !== 'undefined' && indexTable[fields[i]] !== null)
+        {
+          
+          indexTable = indexTable[fields[i]];
+        }
+
+      }
+      if (indexTable !== indexTables && typeof indexTable !== "undefined")
+      {
+        return indexTable;
+      }
+
+      //need to brute force since we do not hit any indexes
       var records = [];
       var i = table.length;
+      var pass = true;
       while (i--)
       {
-        if (table[i][key] === value)
+        pass = true;
+        for(key in keyVal)
+        {
+          if (table[i][key] !== keyVal[key])
+          {
+            pass = false;
+          }
+        }
+        if (pass)
         {
           records.push(table[i]);
         }
@@ -53,16 +109,32 @@ var DataTable = function (fields) {
     },
     count: function () {
       return table.length;
-    }
+    },
+    each: function (fn) {
+      var i;
+      for (i = 0; i < table.length; i++)
+      {
+        fn(i, table[i]);
+      }
+    },
+    createIndex: function (indexFields) {
+      indexes.push(indexFields);
+      //TODO: index all current records
+    },
   };
 };
 
 //setup our event data tables
-var BindTable = new DataTable(['eventName', 'selector', 'fn', 'listenMethod']);
-var TriggerTable = new DataTable(['eventName', 'target', 'fn', 'count']);
-
+var BindTable = new DataTable(['eventName', 'selector', 'fn', 'guid', 'listenMethod']);
+BindTable.createIndex(['eventName', 'guid']);
+var TriggerTable = new DataTable(['eventName', 'target', 'fn', 'guid', 'count']);
+TriggerTable.createIndex(['eventName', 'guid']);
 
 $(BindTable).bind('add', function (event, record) {
-  console.log('Bind logged:', record.eventName, record.selector, record.listenMethod, record);
+  console.log('Bind logged:', record.eventName, record.guid, record.selector, record.listenMethod, record);
+});
+
+$(TriggerTable).bind('add', function (event, record) {
+  console.log('Trigger logged:', record.eventName, record.guid, record.listenMethod, record);
 });
 
--- tests/spec/DataSpec.js
+++ tests/spec/DataSpec.js
@@ -8,7 +8,7 @@ describe('DataTable', function () {
 
   it('can add a bind record row', function () {
     expect(dt.add(record)).toEqual(1);
-    expect(dt.get('eventName', 'click')).toEqual([record]);
+    expect(dt.get({eventName: 'click'})).toEqual([record]);
   });
 
   it('can keep a count of rows added', function () {
@@ -17,12 +17,15 @@ describe('DataTable', function () {
     expect(dt.count()).toEqual(2);
   });
 
+  it('can get a row', function () {
+    expect(dt.get({eventName: 'mouseenter'})).toEqual([record2]);
+  });
+
   it('can remove a row', function () {
     expect(dt.remove('eventName', 'click')).toEqual(1);
     expect(dt.count()).toEqual(1);
-    expect(dt.get('eventName', 'click')).toEqual([]);
-    expect(dt.get('eventName', 'mouseenter')).toEqual([record2]);
+    expect(dt.get({eventName: 'click'})).toEqual([]);
+    expect(dt.get({eventName: 'mouseenter'})).toEqual([record2]);
   });
-
 
 });
