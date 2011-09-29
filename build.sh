02211f73141b7dca2a70674ff455ccd3fa10990b
JD Cantrell <jdcantrell@gmail.com>
2011-09-29 11:50:47 -0700

    Add dataDecoration.js to our build list

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cat src/util.js src/interception.js src/data.js src/main.js src/report.js > triggerFinger.js
+cat src/util.js src/interception.js src/data.js src/main.js src/dataDecoration.js src/report.js > triggerFinger.js
 
-java -jar ../closure-compiler/compiler.jar --js src/util.js --js src/interception.js --js src/data.js --js src/main.js --js src/report.js --js_output_file triggerFinger-min.js
+java -jar ../closure-compiler/compiler.jar --js src/util.js --js src/interception.js --js src/data.js --js src/main.js --js src/dataDecoration.js --js src/report.js --js_output_file triggerFinger-min.js
