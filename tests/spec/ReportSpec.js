3b5b2cc78a68356a93c84e3dbbc7f35cafe4b7c8
JD Cantrell <jdcantrell@gmail.com>
2011-09-30 14:57:45 -0700

    Sync tests with current changes

--- tests/spec/ReportSpec.js
+++ tests/spec/ReportSpec.js
@@ -1,25 +1,31 @@
-describe("Reporting", function() {
-	it("make sure bind still works", function() {
-	  var test = $('<div>');
-	  
-	  test.bind('blah', function () {
-	    expect(true).toEqual(true);
-	  });
-    function testFn() {
-	    expect(false).toEqual(false);
-    }
-	  for (i = 0; i < 100; i++) {
-	    test.bind('blah', testFn);
-	  }
-	  
-	  test.trigger('blah');
-	});
-	
-	it("records when bind is used", function () {
-	  expect(BindTable.count()).toEqual(102);
-	});
+$(document).ready(function () {
+  describe("Reporting", function() {
+    it("make sure bind still works", function() {
+      var test = $('#tester');
+      
+      test.bind('customEvent', function () {
+        expect(true).toEqual(true);
+      });
+      function testFn() {
+        expect(false).toEqual(false);
+      }
+      test.trigger('customEvent');
+      eventCounts = {'click': 0, mouseMove: 0, mouseLeave: 0}
+      test.click(function () {eventCounts.click += 1});
+      test.mousemove(function () {eventCounts.mouseMove += 1});
+      test.mouseleave(function () {eventCounts.mouseLeave += 1});
 
-	it("records when trigger is used", function () {
-	  expect(TriggerTable.count()).toEqual(101);
-	});
+      test.triggerHandler('click');
+    });
+    
+    it("records when bind is used", function () {
+      expect(BindTable.count()).toEqual(4);
+    });
+
+    it("records when trigger is used", function () {
+      expect(TriggerTable.count()).toEqual(2);
+    });
+
+
+  });
 });
